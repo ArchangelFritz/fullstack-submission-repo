a7de5ebf427a3edc9f0d166ba8a567300aae335f
ArchangelFritz <colinfritz@strategiasystems.com>
2025-03-12 10:20:24 -0500

    finished course info, modififed files after for part 2. Mistake

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -1,55 +1,24 @@
-const Header = (course) => {
-  
-  return (
-    <div>
-      <p>{course.course}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.title} count={props.exercises}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.one + props.two + props.three}</p>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.count}</p>
-    </div>
-  )
-}
-
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const [value, setValue] = useState(10)
 
   return (
     <div>
-      <Header course={course}/>
-      <Content title={part1} exercises={exercises1}/>
-      <Content title={part2} exercises={exercises2}/>
-      <Content title={part3} exercises={exercises3}/>
-      <Total one={exercises1} two={exercises2} three={exercises3}/>
+      {value}
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
+
 export default App
+
+
